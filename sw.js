11c02a38f024a601892fbd0d43fc4048e2ff71a8
koncerty-furmanka <k0ncerty.furmanka@gmail.com>
2026-08-08 18:15:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Kôňcerty – service worker
 // Zabezpečuje: rýchlejší štart, offline shell, vždy čerstvé dáta z Gistu, vždy čerstvý HTML shell.
 
-const CACHE = 'koncerty-v12';
+const CACHE = 'koncerty-v13';
 const SHELL = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', (e) => {
