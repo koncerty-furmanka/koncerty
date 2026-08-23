115135a555e0fd5a9d1ca4039531489396220e33
koncerty-furmanka <k0ncerty.furmanka@gmail.com>
2026-08-23 07:35:32 +0200

    Update service worker cache version to v19

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // Kôňcerty – service worker
 // Cache: rýchly štart + offline záloha + obrázky podľa potreby.
 
-const CACHE = 'koncerty-v18';
+const CACHE = 'koncerty-v19';
 const SHELL = ['./', './index.html', './manifest.json', './vinyl.png'];
 
 const DATA_URLS = [
